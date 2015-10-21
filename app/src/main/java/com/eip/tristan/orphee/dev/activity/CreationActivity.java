78fe797815fe2e56097f706beef40dd16213e53d
TristanMoreau <trst.moreau@gmail.com>
2015-10-21 15:24:34 +0200

    drawer.xml updated + splashscreen

---
+++ app/src/main/java/com/eip/tristan/orphee/dev/activity/CreationActivity.java
@@ -0,0 +1,15 @@
+package com.eip.tristan.orphee.dev.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.eip.tristan.orphee.R;
+
+public class CreationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_creation);
+    }
+}
